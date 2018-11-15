4acf40d6655017bd6db39dbf08370b3e8576383d
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-15 23:46:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,61 +1,51 @@
 def welcome
-   puts "Welcome to the Blackjack Table"# code #welcome here
+  # code #welcome here	  puts "Welcome to the Blackjack Table"
+end	
+ def deal_card	def deal_card
+  # code #deal_card here	  rand(1..11)
+end	
+ def display_card_total	def display_card_total(total)
+  # code #display_card_total here	  puts "Your cards add up to #{total}"
+end	
+ def prompt_user	def prompt_user
+  # code #prompt_user here	  puts "Type 'h' to hit or 's' to stay"
+end	
+ def get_user_input	def get_user_input
+  # code #get_user_input here	  gets.chomp
+end	
+ def end_game	def end_game(bust_total)
+  # code #end_game here	  puts "Sorry, you hit #{bust_total}. Thanks for playing!"
+end	
+ def initial_round	def initial_round
+  # code #initial_round here	  result = 0
+end	  2.times do
+     result += deal_card
+def hit?	  end
+  # code hit? here	  display_card_total(result)
+  result
 end
-
-def deal_card
-  number = rand(1..11)
-  return number# code #deal_card here
-end
-
-def display_card_total(number)
-   puts "Your cards add up to #{number}"# code #display_card_total here
-end
-
-def prompt_user
-  puts "Type 'h' to hit or 's' to stay"
-end
-
-def get_user_input
-  input = gets.chomp
-end
-
-def end_game(number)
-   puts "Sorry, you hit #{number}. Thanks for playing!"
-end
-
-def initial_round
-  sum = deal_card + deal_card
-display_card_total(sum)
-return sum
-end
-
-def hit? (number)
+ def hit?(result)
   prompt_user
- answer = get_user_input
- if answer == "h"
-   total = deal_card + number
-   return total
-  elsif answer == 's'
-    return number
+  input = get_user_input
+  if input == 's'
+    result
+  elsif input == 'h'
+    result += deal_card
   else
     invalid_command
-    prompt_user
-end
-
-def invalid_command
-puts "Please enter a valid command"
-end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+    hit?(result)
+  end
+end	
+ def invalid_command	def invalid_command
+  # code invalid_command here	    puts "Invalid command"
+end	  
 
 def runner
-  welcome
-  hand = initial_round
-until hand > 21 do
-  hand = hit?(hand)
-  display_card_total(hand)
-end
-   end_game(hand)
-end
+  # code runner here	  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+end_game(card_total)
+end	
